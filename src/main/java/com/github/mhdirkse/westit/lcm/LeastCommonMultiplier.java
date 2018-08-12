a7cf9f2af6c50359ed18c26ee29a744b41b60831
martijn <mhdirkse@live.nl>
2018-08-13 01:59:57 +0200

    Security does not work, remove. And finish docs.

--- src/main/java/com/github/mhdirkse/westit/lcm/LeastCommonMultiplier.java
+++ src/main/java/com/github/mhdirkse/westit/lcm/LeastCommonMultiplier.java
@@ -50,8 +50,7 @@ import com.google.api.server.spi.config.ApiMethod;
 @Api(
     name = "comGithubMhdirkseWestitLcm",
     version = "v1",
-    description = "Get least common multiplier for divisors 1 ... maxDivisor",
-    scopes = "675305326748-87vcq89jpncl2cuv1siook8j395m0jrc.apps.googleusercontent.com"
+    description = "Get least common multiplier for divisors 1 ... maxDivisor"
 )
 public class LeastCommonMultiplier {
   @ApiMethod(name = "getLeastCommonMultiplier", path = "getLeastCommonMultiplier")
